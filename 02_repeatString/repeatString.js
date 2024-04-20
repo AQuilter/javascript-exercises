273f048971ec1de44979b1874668c3eaca9fb4ed
Allyson Quilter <allysonquilter@gmail.com>
2024-04-20 11:22:32 -0600

    Exercises 01 and 02 complete.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let finalString = "";
 
+    if (num > 0) {
+        for (i = 0; i < num; i++){
+            finalString += string;
+        }
+        return finalString;
+    } else if (num == 0){
+        return "";
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
