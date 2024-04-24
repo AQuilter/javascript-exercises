223ad997c3619017b4687759d8034a5b5a869baa
Allyson Quilter <allysonquilter@gmail.com>
2024-04-23 18:04:14 -0600

    Completed exercises 6, 7, and 8.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  // (x - 32) * 5/9
+  let result = (temp - 32) * (5 / 9);
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temp) {
+    // (x * 9/5 + 32)
+    let result = (temp * (9/5) + 32);
+    return parseFloat(result.toFixed(1));
+  };
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+let total = 0;
+  for (num of array){
+    total = num + total;
+  }
+  return parseInt(total);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  for (num of array){
+    total = num * total;
+  }
+return total;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+	if (num === 0){
+    return 1;
+  } else {
+    for (i = num; i > 0; i--){
+      total = total * i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
